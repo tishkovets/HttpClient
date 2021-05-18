d1207038daccdea61641549f7e6a228e5e449f40
maxim <maxim@tishkovets.ru>
2021-05-19 02:33:36 +0300

    fix isJson

--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,7 @@ namespace HttpClient;
 
 function isJson($string)
 {
-    if (in_array($string[0], ['[', '{'])) {
+    if (in_array((string)$string[0], ['[', '{'])) {
         json_decode($string);
 
         return (json_last_error() == JSON_ERROR_NONE);
